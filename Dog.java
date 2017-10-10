15b50a2a5cc70d27b0e7582c864c3c80a672e033
fazal-029 <31354271+fazal-029@users.noreply.github.com>
2017-10-11 00:05:43 +0600

    Create Dog.java

---
+++ Dog.java
@@ -0,0 +1,18 @@
+package assignment;
+
+public class Dog extends Animal{
+	
+	private int speed;
+	public void name()
+	{
+		System.out.println("this is a dog object..");
+	}
+	public void setSpeed(int _speed)
+	{
+		speed = _speed;
+	}
+	public int getSpeed()
+	{
+		return speed;
+	}
+}
